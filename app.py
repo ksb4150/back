b2520d3ad1abbc12c70504779575bbeb8232395c
김수빈 <38367572+Ksubin0@users.noreply.github.com>
2025-02-07 07:48:44 +0900

    rollback

--- app.py
+++ app.py
@@ -9,10 +9,10 @@ app = Flask(__name__)
 CORS(app)  # React와의 CORS 이슈 해결
 
 # Blueprint 등록
-app.register_blueprint(connect_bp, url_prefix='/api/upbit/')
-app.register_blueprint(trading_bp, url_prefix='/api/trading/')
-app.register_blueprint(openai_bp, url_prefix='/api/openai/')
-app.register_blueprint(crawling_bp, url_prefix='/api/crawling/')
+app.register_blueprint(connect_bp, url_prefix='/api/upbit')
+app.register_blueprint(trading_bp, url_prefix='/api/trading')
+app.register_blueprint(openai_bp, url_prefix='/api/openai')
+app.register_blueprint(crawling_bp, url_prefix='/api/crawling')
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
